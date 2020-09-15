24efa4cc651a27b7bd33724764c534d923b45751
Adam Zverka <adamzverka@hotmail.com>
2020-09-15 14:51:13 +0200

    Added infinite lazily loaded ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My first Flutter app',
+      title: 'Startup Name Generator',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first Flutter app'),
-        ),
-        body: Center(
-          // child:
-          //     Text('Hello World', style: Theme.of(context).textTheme.headline4),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RandomWords(),
-              Text('⬆ randomly generated word pair ⬆'),
-            ],
-          ),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -49,10 +34,40 @@ class RandomWords extends StatefulWidget {
 // in the Dart language and is a recommended best practice for State objects.
 class _RandomWordsState extends State<RandomWords> {
   // Maintains state for the RandomWords widget
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase, style: Theme.of(context).textTheme.headline4);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
-
